4dc1865ad7b4db4e86bbf429b15c36805e30cc29
Rik van Riel <riel@redhat.com>
2016-07-13 16:50:05 +0200

    sched/cputime: Drop local_irq_save/restore from irqtime_account_irq()
    Paolo pointed out that irqs are already blocked when irqtime_account_irq()
    is called. That means there is no reason to call local_irq_save/restore()
    again.
    
    Suggested-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Rik van Riel <riel@redhat.com>
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Radim Krcmar <rkrcmar@redhat.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Wanpeng Li <wanpeng.li@hotmail.com>
    Link: http://lkml.kernel.org/r/1468421405-20056-6-git-send-email-fweisbec@gmail.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: SJD Ayy <1sjd@disroot.org>

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -47,7 +47,6 @@ DEFINE_PER_CPU(seqcount_t, irq_time_seq);
  */
 void irqtime_account_irq(struct task_struct *curr)
 {
-	unsigned long flags;
 	s64 delta;
 	int cpu;
 	u64 wallclock;
@@ -55,8 +54,6 @@ void irqtime_account_irq(struct task_struct *curr)
 
 	if (!sched_clock_irqtime)
 		return;
-
-	local_irq_save(flags);
 
 	cpu = smp_processor_id();
 	wallclock = sched_clock_cpu(cpu);
@@ -84,7 +81,6 @@ void irqtime_account_irq(struct task_struct *curr)
 	else if (curr != this_cpu_ksoftirqd())
 		sched_account_irqstart(cpu, curr, wallclock);
 
-	local_irq_restore(flags);
 }
 EXPORT_SYMBOL_GPL(irqtime_account_irq);
 
