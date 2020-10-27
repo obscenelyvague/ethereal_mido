8b7c17235fb98764bcee209dec22a7e217027813
SJD Ayy <1sjd@disroot.org>
2020-10-27 12:41:42 +0000

    Revert "random: add interrupt callback to VMBus IRQ handler"
    This reverts commit 3e6efc4b219c9312d09eabafb5730f1ef5442a2b.
    
    Signed-off-by: SJD Ayy <1sjd@disroot.org>

--- drivers/hv/vmbus_drv.c
+++ drivers/hv/vmbus_drv.c
@@ -35,7 +35,6 @@
 #include <asm/hyperv.h>
 #include <asm/hypervisor.h>
 #include <asm/mshyperv.h>
-#include <linux/random.h>
 #include "hyperv_vmbus.h"
 
 static struct acpi_device  *hv_acpi_dev;
@@ -690,8 +689,6 @@ static void vmbus_isr(void)
 	/* Check if there are actual msgs to be processed */
 	if (msg->header.message_type != HVMSG_NONE)
 		tasklet_schedule(&msg_dpc);
-
-	add_interrupt_randomness(HYPERVISOR_CALLBACK_VECTOR, 0);
 }
 
 /*
