0b9eb8680e4e4d20df48fea1f7ec5b6e8205ebe8
SJD Ayy <1sjd@disroot.org>
2021-01-01 13:07:00 +0000

    printk: Fix coding standards violation
    Signed-off-by: SJD Ayy <1sjd@disroot.org>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -1647,10 +1647,6 @@ asmlinkage int vprintk_emit(int facility, int level,
 			    const char *dict, size_t dictlen,
 			    const char *fmt, va_list args)
 {
-	/* return instantly if printk is disabled */
-	if (likely(printk_mode == 0))
-		return 0;
-
 	static int recursion_bug;
 	static char textbuf[LOG_LINE_MAX];
 	char *text = textbuf;
@@ -1662,6 +1658,10 @@ asmlinkage int vprintk_emit(int facility, int level,
 	bool in_sched = false;
 	/* cpu currently holding logbuf_lock in this function */
 	static volatile unsigned int logbuf_cpu = UINT_MAX;
+
+	/* return instantly if printk is disabled */
+	if (likely(printk_mode == 0))
+		return 0;
 
 	if (level == SCHED_MESSAGE_LOGLEVEL) {
 		level = -1;
@@ -1838,12 +1838,12 @@ asmlinkage int printk_emit(int facility, int level,
 			   const char *dict, size_t dictlen,
 			   const char *fmt, ...)
 {
+	va_list args;
+	int r;
+
 	/* return instantly if printk is disabled */
 	if (likely(printk_mode == 0))
 		return 0;
-
-	va_list args;
-	int r;
 
 	va_start(args, fmt);
 	r = vprintk_emit(facility, level, dict, dictlen, fmt, args);
